e9f790d35100cf4575902921c0134757d94a37e0
Andersen <51a@ornl.gov>
2020-03-09 16:52:29 -0400

    Added full core file writer to code

--- veraCS.py
+++ veraCS.py
@@ -435,10 +435,14 @@ class Depletion_State(object):
                flow=None,
                inlet=None,
                pressure=None,
+               rodbank=None,
+               restart=False,
                criticality=None
               ):
 
     self.depletion = depletion
+    self.rodbank = rodbank
+    self.restart = restart
     self.power = power
     self.boron = boron
     self.flow = flow
@@ -446,15 +450,382 @@ class Depletion_State(object):
     self.pressure = pressure
     self.criticality = criticality
 
-class Plate(object):
+class Base_Depletion_State(object):
   """
-  Class for plates in a VERA-CS assembly.
+  Base or First Depletion state used to set most of the settings in VERA-CS over the course of the
+  depletion.
   """
   def __init__(self):
-    self.material = None
-    self.thickness = None
-    self.volume_fraction = None
+    self.pressure = None
+    self.symmetry = None 
+    self.feedback = None
+    self.search   = None
+    self.power    = None
+    self.flow     = None
+    self.tinlet   = None
+    self.tinlet_unit = None
+    self.rodbank_names = []
+    self.rodbank_positions = []
+    self.thexp = None
+    self.thexp_tmod = None
+    self.thexp_tclad = None
+    self.thexp_tfuel = None
+    self.thexp_tmod_unit = None
+    self.thexp_tclad_unit = None
+    self.thexp_tfuel_unit = None
 
+class Core(object):
+    """
+    Class for writing full core VERA CS Input Files.
+    """
+    def __init__(self):
+        self.caseID = None
+        self.title = None
+        self.base_state = None
+        self.stateList = {}
+        self.apitch = None
+        self.height = None
+        self.lower_plate = Plate()
+        self.upper_plate = Plate()
+        self.end_spacer_grid = Spacer_Grid()
+        self.mid_spacer_grid = Spacer_Grid()
+        self.power = None
+        self.flow_rate = None
+        self.fuel = {}
+        self.cells = {}
+        self.lattices = {}
+        self.material = {}
+        self.npin = None
+        self.ppitch = None
+        self.axial_lattice_locations = {}
+        self.spacer_grid_locations = {}
+        self.lower_nozzle = Nozzle()
+        self.upper_nozzle = Nozzle()
+        self.axial_mesh = []
+        self.MPACT = {}
+        self.COBRA = {}
+        self.SHIFT = {}
+        self.INSILICO = {}
+
+    def write_file(self):
+        """
+        Function for writing the VERA-CS input file.
+        """
+        file_ = open(self.title+".inp",'w')
+        file_.write("[CASEID]\n")
+        file_.write("  title '{}'\n\n".format(self.caseID))
+        file_.write("[STATE]\n")
+        file_.write("  title '{}'\n".format(self.caseID))
+        file_.write("  pressure {}\n".format(self.base_state.pressure))
+        file_.write("  sym      {}\n".format(self.base_state.symmetry))
+        if self.base_state.feedback:
+          file_.write("  feedback       on\n")
+        else:
+          file_.write("  feedback       off\n")  
+        file_.write("  search   {}\n".format(self.base_state.search))
+        file_.write("\n")
+        count = 0
+        for bank,pos in zip(self.base_state.rodbank_names,self.base_state.rodbank_positions):
+          if not count:
+            file_.write("  rodbank {} {}\n".format(bank,pos))
+          else:
+            file_.write("          {} {}\n".format(bank,pos))
+        file_.write("\n")
+        if self.base_state.thexp:
+          file_.write("  thexp       on\n")
+        else:
+          file_.write("  thexp       off\n")
+        file_.write("  thexp_tmod  {} {}\n".format(self.base_state.thexp_tmod,
+                                                   self.base_state.thexp_tmod_unit))
+        file_.write("  thexp_tclad {} {}\n".format(self.base_state.thexp_tclad,
+                                                   self.base_state.thexp_tclad_unit))
+        file_.write("  thexp_tfuel {} {}\n".format(self.base_state.thexp_tfuel,
+                                                   self.base_state.thexp_tfuel_unit))
+        file_.write("\n")
+        file_.write("        power {}  ; tinlet {} {}\n".format(self.base_state.power,self.base_state.tinlet,
+                                                                self.base_state.tinlet_unit))
+        for state in self.stateList:
+            file_.write("[STATE]\n")
+            if self.stateList[state].power:
+              file_.write("   power {}\n".format(self.stateList[state].power))
+            if self.stateList[state].flow:
+              file_.write("   flow {}\n".format(self.stateList[state].flow))
+            if self.stateList[state].tinlet:
+              file_.write("   tinlet {} {}\n".format(self.stateList[state].tinlet,
+                                                     self.stateList[state].tinlet_units))
+            if self.stateList[state].rodbank:
+              file_.write("   rodbank {} {}\n".format(self.stateList[state].rodbank_name,
+                                                      self.stateList[state].rodbank_position))
+            if self.stateList[state].depletion:
+              file_.write("   deplete {} {} \n".format(self.stateList[state].depletion,
+                                                       self.stateList[state].depletion_units))
+            if self.stateList[state].boron:
+              file_.write("   boron {} \n".format(self.stateList[state].boron))
+            if self.stateList[state].pressure:
+              file_.write("   pressure {}\n".format(self.stateList[state].pressure))
+            if self.stateList[state].restart:
+              file_.write("   restart_write {} {}".format(self.stateList[state].restart_file,state))
+        
+        
+        
+        file_.write("        op_date {}\n".format(self.operating_date))
+        file_.write("\n")
+        file_.write("[CORE]\n")
+        file_.write("  name   WBN\n")
+        file_.write("  unit   1\n")
+        file_.write("  size   {}              ! assemblies across core\n".format(self.size))
+        file_.write("  apitch {}\n".format(self.apitch))
+        file_.write("  rated  {} {}     ! MW, Mlbs/hr\n".format(self.rated_power,self.rated_flow)) 
+        file_.write("  height {}\n".format(height))
+        file_.write("\n")
+        file_.write("  xlabel  R P N M L K J H G  F  E  D  C  B  A\n")
+        file_.write("  ylabel  1 2 3 4 5 6 7 8 9 10 11 12 13 14 15\n")
+        file_.write("\n")
+        file_.write("  core_shape\n")
+        file_.write(self.core_shape)
+        file_.write("\n")
+        file_.write("  assm_map\n")
+        map_string = return_8th_map_string(self.maps['assembly'])
+        file_.write(map_string) 
+        file_.write("\n")
+        file_.write("  insert_map\n")
+        map_string = return_8th_map_string(self.maps['insert'])
+        file_.write(map_string)
+        file_.write("\n")
+        file_.write("  crd_map\n")
+        map_string = return_8th_map_string(self.maps['control'])
+        file_.write(map_string) 
+        file_.write("\n")
+        file_.write("  crd_bank\n")
+        file_.write("    D  -  A  -  D  -  C  -\n")
+        file_.write("    -  -  -  -  - SB  -  -\n")
+        file_.write("    A  -  C  -  -  -  B  -\n")
+        file_.write("    -  -  -  A  - SC  -  -\n")
+        file_.write("    D  -  -  -  D  - SA \n")
+        file_.write("    - SB  - SD  -  -  - \n")
+        file_.write("    C  -  B  - SA  - \n")
+        file_.write("    -  -  -  -\n")
+        file_.write("\n")
+        file_.write("  det_map 193*2\n")
+        file_.write("\n")
+        file_.write("  baffle {} {} {}\n").format(self.baffle.material,self.baffle.lower,self.baffle.upper)) 
+        file_.write("\n")
+        count = 0
+        for mat,pos in zip(self.vessel_material,self.vessel_size):
+          if not count:
+            file_.write("  vessel  {} {}        ! barrel IR (cm)\n".format(mat,pos))
+          else:
+            file_.write("           {} {}\n".format(mat,pos))        
+        file_.write("\n")
+        file_.write("  pad ss  194.64 201.63 32 45 135 225 315 \n")! neutron pad ID,OD arc lenth (degrees), and angular positions (degrees)
+        file_.write("\n")
+        file_.write("  lower_plate {}  {} {}   ! mat, thickness, vol frac\n".format(self.lower_plate.material,
+                                                                                    self.lower_plate.thickness,
+                                                                                    self.lower_plate.volume_fraction))
+        file_.write("  upper_plate {}  {} {}   ! mat, thickness, vol frac\n".format(self.upper_plate.material,
+                                                                                      self.upper_plate.thickness,
+                                                                                      self.upper_plate.volume_fraction))
+        file_.write("\n")
+        file_.write("\n")
+        file_.write("[ASSEMBLY]\n")
+        file_.write('  title "Westinghouse 17x17"\n')
+        file_.write("  npin   {}\n".format(self.npin))
+        file_.write("  ppitch {}\n".format(self.pinpitch))
+        file_.write("  \n")
+        file_.write("\n")
+        for fu in self.fuel:
+          file_.write("  fuel {}".format(fu))
+          file_.write(" {}".format(self.fuel[fu].density))
+          file_.write(" {} / ".format(self.fuel[fu].theory_dense))
+          file_.write("{} ".format(self.fuel[fu].enrichment))
+          if not self.fuel[fu].components:
+            pass
+          else:
+            for comp in self.fuel[fu].components:
+              file_.write(" {} ".format(comp))
+          if not self.fuel[fu].gad_concentration:
+            pass
+          else:
+            file_.write(" / {}={}".format(self.fuel[fu].gad_material,self.fuel[fu].gad_concentration))
+        file_.write("\n")
+        for cell in self.cells:
+          file_.write("  cell {}   ".format(cell))
+          for radius in self.cells[cell]['radius']:
+            file_.write(" {}".format(radius))
+          file_.write(" / ")
+          for material in self.cells[cell]['material']:
+            file_.write("{} ".format(material))
+          if 'description' in self.cells[cell]:
+            file_.write(" ! {}".format(self.cells[cell]['description']))
+          file_.write("\n")
+        file_.write("\n")
+        for lattice in self.lattices:
+          file_.write("  lattice {}\n".format(lattice))
+          triangular_string = return_triangular_string(self.lattices[lattice],"       ")
+          file_.write(triangular_string)
+          file_.write("\n")
+        for ax in self.axial_lattice_dict:
+          str_ = "  axial  {}".format(ax)
+          for pos,lat in zip(self.axial_lattice_dict[ax].height[-1],self.axial_lattice_dict[ax].lattice):
+            str_ += "  {}  {}".format(pos,lat)
+          str_ += "  {}\n".format(self.axial_lattice_dict[ax].height[-1])
+          file_.write(str_)
+        file_.write("\n")
+        file_.write("  grid END {}    {} {}\n".format(self.end_spacer_grid.material,
+                                                      self.end_spacer_grid.height,
+                                                      self.end_spacer_grid.mass))
+        file_.write("  grid MID {}  {} {}\n".format(self.mid_spacer_grid.material,
+                                                    self.mid_spacer_grid.height,
+                                                    self.mid_spacer_grid.mass)) 
+        file_.write("\n")
+        file_.write("  grid_axial\n")
+        for i,j in zip(self.spacer_grid_locations['order'],self.spacer_grid_locations['height']):
+          file_.write("      {}  {}\n".format(i,j))
+        file_.write("\n")
+        file_.write("  lower_nozzle  {} {} {}  ! mat, height, mass (g)\n".format(self.lower_nozzle.material,
+                                                                                 self.lower_nozzle.height,
+                                                                                 self.lower_nozzle.mass))
+        file_.write("  upper_nozzle  {} {} {}  ! mat, height, mass (g)\n".format(self.upper_nozzle.material,
+                                                                                 self.upper_nozzle.height,
+                                                                                 self.upper_nozzle.mass))
+        file_.write("\n")
+        if self.insert:
+          file_.write("[INSERT]\n")
+          file_.write('  title "{}"\n'.format(self.insert.title))
+          file_.write("  npin {}\n".format(self.insert.npin))
+          file_.write("\n")
+          for cell in self.insert.cells:
+            file_.write("  cell {}".format(cell))
+            str_ = ""
+            for rad in self.insert.cells[cell]['radius']:
+              str_ += " {}".format(rad)
+            str_ += " /"
+            for mad in self.insert.cells[cell]['material']:
+              str_ += " {}".format(mad)
+            str_ += " ! {}\n".format(self.insert.cells[cell]['description'])
+            file_.write(str_)
+          file_.write("\n")
+          for lattice in self.insert.lattices:
+            file_.write("  lattice {}\n".format(lattice))
+            triangular_string = return_triangular_string(self.insert.lattices[lattice],"       ")
+            file_.write(triangular_string)
+            file_.write("\n")
+          file_.write("\n")
+          for ax in self.insert.axial_lattice_dict:
+            str_ = "  axial  {}".format(ax)
+            for pos,lat in zip(self.insert.axial_lattice_dict[ax].height[-1],
+                               self.insert.axial_lattice_dict[ax].lattice):
+              str_ += "  {}  {}".format(pos,lat)
+            str_ += "  {}\n".format(self.insert.axial_lattice_dict[ax].height[-1])
+            file_.write(str_)
+          file_.write("\n")
+        if self.control:
+          file_.write("[CONTROL]\n")
+          file_.write('  title "{}"\n'.format(self.control.title))
+          file_.write("  npin    {}\n".format(self.control.npin))
+          file_.write("  stroke  {}  {}    ! approx for 1.5875 step sizes and 230 max stroke".format(self.control.stroke_one,
+                                                                                                     self.control.stroke_two))
+          file_.write("\n")
+          for cell in self.control.cells:
+            file_.write("  cell {}   ".format(cell))
+            for radius in self.control.cells[cell]['radius']:
+              file_.write(" {}".format(radius))
+            file_.write(" / ")
+            for material in self.control.cells[cell]['material']:
+              file_.write("{} ".format(material))
+            if 'description' in self.cells[cell]:
+              file_.write(" ! {}".format(self.cells[cell]['description']))
+            file_.write("\n")
+          file_.write("\n")
+          for lattice in self.control.lattices:
+            file_.write("  lattice {}\n".format(lattice))
+            triangular_string = return_triangular_string(self.control.lattices[lattice],"       ")
+            file_.write(triangular_string)
+            file_.write("\n")
+          file_.write("\n")
+          for ax in self.control.axial_lattice_dict:
+            str_ = "  axial  {}".format(ax)
+            for pos,lat in zip(self.control.axial_lattice_dict[ax].height[-1],
+                               self.control.axial_lattice_dict[ax].lattice):
+              str_ += "  {}  {}".format(pos,lat)
+            str_ += "  {}\n".format(self.control.axial_lattice_dict[ax].height[-1])
+            file_.write(str_)
+          file_.write("\n")
+          file_.write("\n")
+        if self.detector:
+          file_.write("[DETECTOR]\n")
+          file_.write('  title "{}"\n'.format(self.detector.title))
+          file_.write("  npin {}\n".format(self.detector.npin))
+          file_.write("\n")
+          file_.write("\n")
+          for cell in self.detector.cells:
+            file_.write("  cell {}   ".format(cell))
+            for radius in self.detector.cells[cell]['radius']:
+              file_.write(" {}".format(radius))
+            file_.write(" / ")
+            for material in self.detector.cells[cell]['material']:
+              file_.write("{} ".format(material))
+            if 'description' in self.detector.cells[cell]:
+              file_.write(" ! {}".format(self.detector.cells[cell]['description']))
+            file_.write("\n")
+          file_.write("\n")
+          for lattice in self.detector.lattices:
+            file_.write("  lattice {}\n".format(lattice))
+            triangular_string = return_triangular_string(self.insert.lattices[lattice],"       ")
+            file_.write(triangular_string)
+            file_.write("\n")
+          file_.write("\n")
+          for ax in self.detector.axial_lattice_dict:
+            str_ = "  axial  {}".format(ax)
+            for pos,lat in zip(self.detector.axial_lattice_dict[ax].height[-1],
+                               self.detector.axial_lattice_dict[ax].lattice):
+              str_ += "  {}  {}".format(pos,lat)
+            str_ += "  {}\n".format(self.detector.axial_lattice_dict[ax].height[-1])
+            file_.write(str_)
+          file_.write("\n")
+          file_.write("\n")
+        file_.write("[EDITS]\n")
+        file_.write("  axial_edit_bounds   ! problem 10 mesh for 4.0_RC7\n")
+        for axial in self.axial_edit_bounds:
+          file_.write("       {}\n".format(axial))
+        file_.write("\n")
+        file_.write("\n")
+        if not self.MPACT:
+          pass
+        else:
+          file_.write("[MPACT]\n")
+          for key in self.MPACT:
+            file_.write("  {} {}\n".format(key,self.MPACT[key]))
+        file_.write("\n")
+        if not self.COBRA:
+          pass
+        else:
+          file_.write("[COBRATF]\n")
+          for key in self.COBRA:
+            file_.write("  {} {}\n".format(key,self.COBRA[key]))
+        file_.write("\n")
+        if not self.SHIFT:
+          pass
+        else:
+          file_.write("[SHIFT]\n")
+          for key in self.SHIFT:
+            file_.write("  {} {}\n".format(key,self.SHIFT[key]))
+        file_.write("\n")
+        if self.run:
+        for key in self.run:
+            file_.write("  {} {}\n".format(key,self.run[key]))
+        file_.write("\n")
+        file_.close()
+
+class Plate(object):
+    """
+    Class for plates in a VERA-CS assembly.
+    """
+    def __init__(self):
+      self.material = None
+      self.thickness = None
+      self.volume_fraction = None
+      
 class Fuel(object):
   """
   Class for designating fuel types. Right now for VERA-CS. 
@@ -579,6 +950,26 @@ def return_state_list(file_name):
       state_list.append(key)
 
   return state_list
+
+def return_8th_map_string(list_):
+  """
+  Returns a map using 8th core symmetry for a 15 x 15 core map.
+  """
+  if len(list_) == 31:
+    str_ =  "    {}\n".format(list_[0])
+    str_ += "    {} {}\n".format(list_[1],list_[2])
+    str_ += "    {} {} {}\n".format(list_[3],list_[4],list_[5])
+    str_ += "    {} {} {} {}\n".format(list_[6],list_[7],list_[8],list_[9])
+    str_ += "    {} {} {} {} {}\n".format(list_[10],list_[11],list_[12],list_[13],list_[14])
+    str_ += "    {} {} {} {} {} {}\n".format(list_[15],list_[16],list_[17],list_[18],list_[19],list_[20])
+    str_ += "    {} {} {} {} {} {}\n".format(list_[21],list_[22],list_[23],list_[24],list_[25],list_[26]) 
+    str_ += "    {} {} {} {} \n".format(list_[27],list_[28],list_[29],list_[30])
+
+    return str_
+
+  else:
+    error_ = "Map list is of length {} instead of length 31".format(len(list_))
+    return ValueError(error_)
 
 if __name__ == "__main__":
   pass
