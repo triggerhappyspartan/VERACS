a9095f2649adfc39fe8c786bd2ce806f24eaace2
triggerhappyspartan <bdander3@ncsu.edu>
2020-08-30 20:19:00 -0400

    new file added and changes to compiler

--- new_compile_library.py
+++ new_compile_library.py
@@ -8,6 +8,10 @@ def extract_data(case):
     """
     Extracts the data from the case and returns it as a series of numpy matrices.
     """
+    state_list = ['STATE_0001','STATE_0002','STATE_0003','STATE_0004','STATE_0005','STATE_0006','STATE_0007','STATE_0008',
+                  'STATE_0009','STATE_0010','STATE_0011','STATE_0012','STATE_0013','STATE_0014','STATE_0015','STATE_0016',
+                  'STATE_0017','STATE_0018','STATE_0019','STATE_0020','STATE_0021','STATE_0022','STATE_0023','STATE_0024',
+                  'STATE_0025']
     key_params = ['cool_boron','cool_nickel_particulate','pin_powers','pin_avg_crud_massdensity','pin_avg_crud_thickness']
     position_map = {0:{'x':[0,17],    'y':[0,17]},     1:{'x':[17,34],   'y':[0,17]},
                     2:{'x':[34,51],   'y':[0,17]},     3:{'x':[51,68],   'y':[0,17]},
@@ -38,51 +42,77 @@ def extract_data(case):
                     52:{'x':[0,17],   'y':[119,136]},  53:{'x':[17,34],  'y':[119,136]},
                     54:{'x':[34,51],  'y':[119,136]},  55:{'x':[51,68],  'y':[119,136]}}
     
-    all_params = True
-    for key in key_params:
-        if key not in case.keys():
-            all_params = False    
-    if all_params:    
-        for assem in position_map:
-            x1 = position_map[assem]['x'][0]
-            x2 = position_map[assem]['x'][1]
-            y1 = position_map[assem]['y'][0]
-            y2 = position_map[assem]['y'][1]    
-            regular_pin_matrix[tcc,x1:x2,y1:y2,:] = case['pin_powers'][:,:,:,assem]
-            crud_matrix[tcc,x1:x2,y1:y2,:]  = case['pin_avg_crud_massdensity'][:,:,:,assem]
-        core_parameters[tcc,0] = case['cool_boron'][()]
-        core_parameters[tcc,1] = case['cool_nickel_particulate'][()]
-        core_parameters[tcc,2] = case['EFPD'][()] 
+    for i,state in enumerate(state_list[1:]):
+        if state in case.keys():
+            previous_state = state_list[i]
+            if previous_state in case.keys():
+                all_params = True
+                for key in key_params:
+                    if key not in case[state].keys():
+                        all_params = False
+                    elif key not in case[previous_state].keys():
+                        all_params = False
+                if all_params:    
+                    for assem in position_map:
+                        x1 = position_map[assem]['x'][0]
+                        x2 = position_map[assem]['x'][1]
+                        y1 = position_map[assem]['y'][0]
+                        y2 = position_map[assem]['y'][1]    
+                        for i in range(49):
+                            integrated_pin_matrix[tcc,x1:x2,y1:y2,i] = numpy.divide(numpy.sum(case[state]['pin_powers'][:,:,:i,assem],axis=2),
+                                                                                    numpy.sum(case[state]['pin_powers'][:,:,:,assem],axis=2))
+                        regular_pin_matrix[tcc,x1:x2,y1:y2,:] = case[state]['pin_powers'][:,:,:,assem]
+                        crud_input_matrix[tcc,x1:x2,y1:y2,:]  = case[previous_state]['pin_avg_crud_massdensity'][:,:,:,assem]
+                        crud_output_matrix[tcc,x1:x2,y1:y2,:] = case[state]['pin_avg_crud_massdensity'][:,:,:,assem]
+                        crud_diff_matrix[tcc,x1:x2,y1:y2,:] = case[state]['pin_avg_crud_massdensity'][:,:,:,assem] - case[previous_state]['pin_avg_crud_massdensity'][:,:,:,assem]
+                    core_parameters[tcc,0] = case[state]['cool_boron']
+                    core_parameters[tcc,1] = case[state]['cool_nickel_particulate']
+                    core_parameters[tcc,2] = case[state]['EFPD']/365.
+                    state_list[tcc] = state
+                    tcc += 1
 
 if __name__ == "__main__":
-    data_file = h5py.File('E:cycle_10_compiled.h5','r')
+    file_list = ['E:massive_one.h5','E:massive_two.h5','E:massive_three.h5','E:second_library.h5']
     
     state_list = ['STATE_0001','STATE_0002','STATE_0003','STATE_0004','STATE_0005','STATE_0006','STATE_0007','STATE_0008',
                   'STATE_0009','STATE_0010','STATE_0011','STATE_0012','STATE_0013','STATE_0014','STATE_0015','STATE_0016',
                   'STATE_0017','STATE_0018','STATE_0019','STATE_0020','STATE_0021','STATE_0022','STATE_0023','STATE_0024',
                   'STATE_0025']
 
-    count = 0
-    key_list = list(data_file.keys())
-    regular_pin_matrix = numpy.zeros([5,17*8,17*8,49])
-    crud_matrix  = numpy.zeros([5,17*8,17*8,49])
-    core_parameters    = numpy.zeros([5,3])
+    matrix_parameter_list = ['previous_pin_powers','pin_powers','pin_avg_crud_massdensity','pin_avg_crud_borondensity']
+    single_parameter_list = ['EFPD','cool_boron','cool_nickel_particulate']
 
-    outfile = h5py.File('E:short_library.h5','w')
-    data_list = list(data_file.keys())
-    for state in state_list:
-        for tcc,key in enumerate(data_list[:5]):
-            if state in data_file[key]:
-                extract_data(data_file[key][state])
-        g1 = outfile.create_group(state)    
-        g1.create_dataset('pin_in',data=regular_pin_matrix)
-        g1.create_dataset('crud',data=crud_matrix)
-        g1.create_dataset('core_in',data=core_parameters)
-        regular_pin_matrix[:,:,:,:] = 0. 
-        crud_matrix[:,:,:,:] = 0.
-        core_parameters[:,:] = 0.
+    new_library = h5py.File('Final_library.h5','r')
+    tcc = 0
+    for file_ in file_list:
+        data_file = h5py.File(file_,'r')
+        for group in file_.key():
+            for case in file_[group].keys():
+                h5_case = file_[group][case]
+                extract_data(h5_case)
+        data_file.close()
 
-    outfile.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
